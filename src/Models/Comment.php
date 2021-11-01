2a3b5a237fd88efb8cd5ae9a9da40da1731d3095
giscard <giscarddesousajunior@hotmail.fr>
2021-11-01 15:59:25 +0100

    ajout du model Comment.php & diagramme de classe

---
+++ src/Models/Comment.php
@@ -0,0 +1,120 @@
+<?php
+
+
+namespace App\Models;
+
+
+class Comment
+{
+
+    protected $id;
+
+    protected $content;
+
+    protected $isValid;
+
+    /**
+     * @return mixed
+     */
+    public function getIsValid()
+    {
+        return $this->isValid;
+    }
+
+    /**
+     * @param mixed $isValid
+     */
+    public function setIsValid($isValid): void
+    {
+        $this->isValid = $isValid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostId()
+    {
+        return $this->postId;
+    }
+
+    /**
+     * @param mixed $postId
+     */
+    public function setPostId($postId): void
+    {
+        $this->postId = $postId;
+    }
+
+    protected $postId;
+
+    protected $auther;
+
+    protected $dateValidation;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuther()
+    {
+        return $this->auther;
+    }
+
+    /**
+     * @param mixed $auther
+     */
+    public function setAuthor($auther): void
+    {
+        $this->author = $auther;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateValidation()
+    {
+        return $this->dateValidation;
+    }
+
+    /**
+     * @param mixed $dateValidation
+     */
+    public function setUpdateDate($dateValidation): void
+    {
+        $this->dateValidation = $dateValidation;
+    }
+
+
+}
